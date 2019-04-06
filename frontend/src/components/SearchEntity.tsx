cb92af596637ed1feb1ed8c14903f90c1802f31f
Nathan Vogel <nathan@nathanvogel.com>
2019-04-06 21:38:17 +0200

    :sparkles: Autocomplete search box :D

---
+++ frontend/src/components/SearchEntity.tsx
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+import * as React from "react";
+import AsyncSelect from "react-select/lib/Async";
+import axios from "axios";
+
+const suggestionsApi = axios.create({
+  baseURL: "http://127.0.0.1:8529/_db/_system/api1",
+  headers: {}
+});
+
+export interface Suggestion {
+  _key: string;
+  name: string;
+}
+export interface ReactSelectOption {
+  value: string;
+  label: string;
+}
+export interface ReactSelectInputValue {
+  inputValue: string;
+}
+
+const promiseAutocomplete = async (inputValue: string) => {
+  // No need to query the server too fast
+  if (!inputValue || inputValue.length <= 2) return;
+  // Query our beautiful API
+  const response = await suggestionsApi.get(
+    "/entities/autocomplete/" + inputValue
+  );
+  if (response.status === 200) {
+    // Convert the API data to react-select format.
+    const suggestions: Array<ReactSelectOption> = [];
+    const data = response.data as Array<Suggestion>;
+    for (var i = 0; i < data.length; i += 1) {
+      suggestions.push({
+        value: data[i]._key,
+        label: data[i].name
+      });
+    }
+    return suggestions;
+  } else {
+    console.error("Error requesting suggestions: " + response.status);
+    console.error(response);
+    return [];
+  }
+};
+
+const StyledSelect = styled(AsyncSelect)`
+  min-width: 240px;
+
+  .rs__indicators {
+    display: none;
+  }
+`;
+
+export interface Props {
+  onChange?: (value: string) => void;
+  className?: string;
+}
+
+// object is the state type
+class SearchEntity extends React.Component<Props, object> {
+  onChange = (object: any) => {
+    if (this.props.onChange) this.props.onChange(object.value);
+  };
+
+  render() {
+    return (
+      <StyledSelect
+        className={this.props.className}
+        cacheOptions
+        defaultOptions
+        classNamePrefix="rs"
+        onChange={this.onChange}
+        noOptionsMessage={(d: ReactSelectInputValue) => {
+          return d.inputValue && d.inputValue.length > 2
+            ? "No corresponding person"
+            : null;
+        }}
+        placeholder="Search..."
+        loadOptions={promiseAutocomplete}
+      />
+    );
+  }
+}
+
+export default SearchEntity;
--- frontend/src/screens/App.tsx
+++ frontend/src/screens/App.tsx
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
 import Button from "../components/Button";
-import logo from "./logo.svg";
+import SearchEntity from "../components/SearchEntity";
+import styled from "styled-components";
 import "./App.css";
+
+const CentralSearch = styled(SearchEntity)`
+  font-size: 22px;
+  width: 330px;
+  max-width: 100%;
+  display: block;
+  margin: 20px auto;
+`;
+
+const Header = styled.header`
+  width: 960px;
+  max-width: calc(100% - 64px);
+  margin-top: 20vh;
+  margin-left: 32px;
+  margin-right: 32px;
+`;
+
+const CentralTitle = styled.h2`
+  width: 100%;
+  text-align: center;
+  font-size: 24px;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <Header>
+          <CentralTitle>Explore the relation graph</CentralTitle>
+          <CentralSearch />
           <Button>Button test</Button>
-        </header>
+        </Header>
       </div>
     );
   }
