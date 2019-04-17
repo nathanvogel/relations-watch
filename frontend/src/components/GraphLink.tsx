2dc4d133954d9ac6ac62c9c8ad9a2d719e5e8d8e
Nathan Vogel <nathan@nathanvogel.com>
2019-04-18 01:31:27 +0200

    :lipstick: Links are multiplied and colored!

--- frontend/src/components/GraphLink.tsx
+++ frontend/src/components/GraphLink.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { LinkRenderData } from "../utils/types";
+import { RELATION_COLORS } from "../utils/consts";
 
 type OwnProps = {
   data: LinkRenderData;
@@ -9,11 +10,27 @@ type OwnProps = {
 class GraphLink extends React.Component<OwnProps> {
   render() {
     const { x1, y1, x2, y2, angle, types, entityKey } = this.props.data;
-    const coords = { x1, y1, x2, y2 };
+    const dx: number[] = [];
+    const dy: number[] = [];
+    for (let i = 0; i < types.length; i += 1) {
+      const offset = i - (types.length - 1) / 2; // % 2 === 0 ? -i / 2 : (i + 1) / 2;
+      dx.push(Math.sin(angle + 90) * offset * 3);
+      dy.push(Math.cos(angle + 90) * offset * 3);
+    }
 
     return (
       <g id={"edges-" + entityKey}>
-        <line {...coords} stroke="#999" strokeWidth={2} />
+        {types.map((type, index) => (
+          <line
+            key={type}
+            x1={x1 + dx[index]}
+            y1={y1 + dy[index]}
+            x2={x2 + dx[index]}
+            y2={y2 + dy[index]}
+            stroke={RELATION_COLORS[type]}
+            strokeWidth={1.5}
+          />
+        ))}
       </g>
     );
   }
