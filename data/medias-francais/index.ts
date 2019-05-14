7ddbf5c68a0d47a0a18503978ee96f61db7bc0e9
Nathan Vogel <nathan@nathanvogel.com>
2019-05-14 15:03:08 +0200

    :sparkles: :wrench: Communication with local database

--- data/medias-francais/index.ts
+++ data/medias-francais/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+import { Database, aql } from "arangojs";
+
+const db = new Database({
+  url: "http://localhost:8529"
+});
+db.useDatabase("_system");
+db.useBasicAuth("root", "");
+
+(async function() {
+  const now = Date.now();
+  try {
+    const cursor = await db.query(aql`
+      RETURN ${now}
+    `);
+    const result = await cursor.next();
+    console.log("Result:", result);
+  } catch (err) {
+    console.error(err.stack);
+  }
+})();
