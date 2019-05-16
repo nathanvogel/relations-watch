0ea26cba39a6845763127fc9d9f4c302b31bed8c
Nathan Vogel <nathan@nathanvogel.com>
2019-05-17 01:53:40 +0200

    :sparkles: Add some basic project info + autodetect/change language

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -1,19 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./en/strings.json";
+import fr from "./fr/strings.json";
 
 // The translations
 const resources = {
-  en: {
-    translation: en
-  }
+  en: { translation: en },
+  fr: { translation: fr }
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en",
+    fallbackLng: "en",
     keySeparator: false, // we do not use keys in form messages.welcome
     interpolation: {
       escapeValue: false // react already safes from xss
--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,15 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, FunctionComponent } from "react";
 import { RouterProps } from "react-router";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useTranslation, Trans } from "react-i18next";
 
 import EntitySearch from "../components/EntitySearch";
 import ROUTES from "../utils/ROUTES";
 import { ReactSelectOption } from "../utils/types";
 import { TP } from "../utils/theme";
+import R from "../strings/R";
 
 const Header = styled.header`
   margin-top: 16vh;
 `;
+
+const Footer = styled.footer``;
 
 const CentralSearch = styled(EntitySearch)`
   font-size: 22px;
@@ -17,6 +22,7 @@ const CentralSearch = styled(EntitySearch)`
   width: 330px;
   max-width: 100%;
   margin: 0 auto;
+  margin-bottom: 12vh;
 
   .rs__control {
     padding: 4px ${(props: TP) => props.theme.inputPaddingLR};
@@ -28,21 +34,35 @@ const CentralTitle = styled.h2`
   font-size: 24px;
 `;
 
-class HomeScreen extends Component<RouterProps> {
-  onSearch = (searchSelection: ReactSelectOption) => {
+const HomeScreen: FunctionComponent<RouterProps> = props => {
+  const { t } = useTranslation();
+
+  const onSearch = (searchSelection: ReactSelectOption) => {
     const entityKey = searchSelection.value;
     const url = `/${ROUTES.entity}/${entityKey}`;
-    this.props.history.push(url);
+    props.history.push(url);
   };
 
-  render() {
-    return (
+  return (
+    <div>
       <Header>
         <CentralTitle>Explore the relation graph</CentralTitle>
-        <CentralSearch onChange={this.onSearch} />
+        <CentralSearch onChange={onSearch} />
+        <h2>{t(R.slogan)}</h2>
+        <h3>{t(R.faq_about_q)}</h3>
+        <p>{t(R.faq_about_a)}</p>
+        <h3>{t(R.faq_data_q)}</h3>
+        <p>
+          <Trans i18nKey={R.faq_data_a}>
+            <a href="https://www.monde-diplomatique.fr/cartes/PPA" />
+          </Trans>
+        </p>
       </Header>
-    );
-  }
-}
+      <Footer>
+        <a href="/?lng=en">EN</a> / <a href="/?lng=fr">FR</a>
+      </Footer>
+    </div>
+  );
+};
 
 export default HomeScreen;
