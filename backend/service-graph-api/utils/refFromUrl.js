9f4449557b4e21443b21407b0fc8a6844628f6c7
Nathan Vogel <nathan@nathanvogel.com>
2019-05-19 14:10:21 +0200

    :sparkles: Remove hash from URLs to ref

--- backend/service-graph-api/utils/refFromUrl.js
+++ backend/service-graph-api/utils/refFromUrl.js
@@ -9,13 +9,30 @@ function getSimplifiedLink(fullUrl, parsedUrl, rootDomain) {
   if (!rootDomain) rootDomain = getRootDomain(parsedUrl.hostname);
 
   // Uniform Youtube links
-  fullUrl = getUniformYoutubeLink(fullUrl, parsedUrl, rootDomain);
-
-  // Remove protocol
-  var ref = fullUrl.replace(/^https?:\/\//, "");
-
-  // TODO : remove hash
+  var url = getUniformYoutubeLink(fullUrl, parsedUrl, rootDomain);
+  // Uniform links with #anchors
+  url = removeHash(url, parsedUrl);
+  // Remove protocol last, otherwise it's potentially hard to parse the url.
+  var ref = url.replace(/^https?:\/\//, "");
   return ref;
+}
+
+/**
+ * Like removeHash(), but precomputes args.
+ * This function is intended for test usage only.
+ */
+function removeHashFromStr(fullUrl) {
+  const parsedUrl = new UrlParse(fullUrl);
+  return removeHash(fullUrl, parsedUrl);
+}
+
+function removeHash(fullUrl, parsedUrl) {
+  if (!parsedUrl.hash) return fullUrl;
+  return removeLast(fullUrl, parsedUrl.hash);
+}
+
+function removeLast(str, searchTerm) {
+  return str.replace(new RegExp(searchTerm + "$"), "");
 }
 
 /**
@@ -42,23 +59,11 @@ function getUniformYoutubeLink(fullUrl, parsedUrl, rootDomain) {
   }
 }
 
-function getUniformYoutubeLink_Regexp(fullUrl, parsedUrl, rootDomain) {
-  if (!rootDomain) return fullUrl;
-  if (!parsedUrl) return fullUrl;
-  if (rootDomain !== "youtube.com" && rootDomain !== "youtu.be") return fullUrl;
-
-  var matches = fullUrl.match(youtubeRegexp);
-  console.log("Matches:", matches);
-  for (let match of matches) {
-    if (match && match.length == 11) {
-      return "https://www.youtube.com/watch?v=" + match;
-    }
-  }
-}
-
 module.exports = {
   getSimplifiedLink,
   simplifyYoutubeUrl,
   getUniformYoutubeLink,
-  getUniformYoutubeLink_Regexp
+  removeHashFromStr,
+  removeHash
+  // getUniformYoutubeLink_Regexp
 };
--- backend/service-graph-api/utils/refFromUrl.test.js
+++ backend/service-graph-api/utils/refFromUrl.test.js
@@ -1,7 +1,8 @@
-import { simplifyYoutubeUrl } from "./refFromUrl";
+import { simplifyYoutubeUrl, removeHashFromStr } from "./refFromUrl";
 
 // For proper parsing, the URL protocol is required.
 // (the user will also need to put it).
+// No need to test for basic URL validity here.
 
 const targetUrl = "https://www.youtube.com/watch?v=wR-JxJ9II78";
 const validUrls = [
@@ -17,7 +18,7 @@ const validUrls = [
 ];
 const invalidUrls = [
   "https://www.fakeyoutube.com/watch?v=wR-JxJ9II78",
-  "www.fakeyoutube.com/watch?v=wR-JxJ9II78",
+  "https://www.fakeyoutube.com/wrath?v=wR-JxJ9II78",
   "https://youtu.bee/wR-JxJ9II78"
 ];
 
@@ -31,4 +32,19 @@ test("test invalid youtube urls", () => {
   for (let url of invalidUrls) {
     expect(simplifyYoutubeUrl(url)).toBe(url);
   }
+});
+
+test("remove hash", () => {
+  expect(
+    removeHashFromStr("https://github.com/unshiftio/url-parse#urlsetkey-value")
+  ).toBe("https://github.com/unshiftio/url-parse");
+  expect(removeHashFromStr("https://github.com/unshiftio/url-parse#")).toBe(
+    "https://github.com/unshiftio/url-parse"
+  );
+  expect(removeHashFromStr("https://mail.google.com/mail/u/0/#inbox")).toBe(
+    "https://mail.google.com/mail/u/0/"
+  );
+  expect(removeHashFromStr("https://mail.google.com/#mail/u/0/#inbox")).toBe(
+    "https://mail.google.com/"
+  );
 });
