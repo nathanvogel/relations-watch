1804f28123a574f562a79f6913f17980c72a5dad
Nathan Vogel <nathan@nathanvogel.com>
2019-06-11 14:54:15 +0200

    :lipstick: deduplicated link summary

--- frontend/src/components/BigLinksPreview.tsx
+++ frontend/src/components/BigLinksPreview.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styled from "styled-components";
-import { useTranslation } from "react-i18next";
 import { edgeTextResKey } from "../strings/strings";
 import { edgeColor } from "../styles/theme";
 import { getInbetweenEdges } from "../features/linksSelector2";
 import { connect } from "react-redux";
 import { isDirectedType } from "../utils/utils";
 import { RootStore } from "../Store";
+import { LinkDir, EdgePreview, RelationType } from "../utils/types";
+import i18n from "../i18n/i18n";
 
 const EdgeTypeExplainer = styled.div`
   padding: 1px 4px;
+  box-sizing: border-box;
   border-radius: 0px;
   font-size: ${props => props.theme.fontSizeS};
   font-weight: bold;
@@ -18,10 +20,25 @@ const EdgeTypeExplainer = styled.div`
   width: 100%;
   max-width: 100%;
   height: auto;
+  // To position:absolute the children
+  position: relative;
+
+  & > span.left {
+    position: absolute;
+    left: 8px;
+    top: 2px;
+  }
+
+  & > span.right {
+    position: absolute;
+    right: 8px;
+    top: 2px;
+  }
 `;
 
 const ExplainersWrapper = styled.div`
   margin: 4px 0px;
+  box-sizing: border-box;
   text-align: center;
   font-size: ${props => props.theme.fontSizeS};
 `;
@@ -42,38 +59,73 @@ const mapDispatchToProps = () => ({});
 type Props = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
 
+type RTypeSummary = {
+  direction: LinkDir.None | LinkDir.Normal | LinkDir.Invert;
+  color: string;
+  text: string; // will factor past/owned/etc.
+  type: RelationType; // for sorting
+  key: string; // for React
+};
+
+const getSummary = (edge: EdgePreview, sourceKey: string): RTypeSummary => {
+  // WARNING: unpure 't' injection, but who cares?
+  const directed = isDirectedType(edge.type, edge.fType);
+  return {
+    text: i18n.t(edgeTextResKey(edge.type, edge.fType)),
+    color: edgeColor(edge.type, edge.fType),
+    direction: directed
+      ? edge._from === sourceKey
+        ? LinkDir.Invert
+        : LinkDir.Normal
+      : LinkDir.None,
+    type: edge.type,
+    key: edge._key,
+  };
+};
+
+const areSummariesEquivalent = (
+  s1: RTypeSummary,
+  s2: RTypeSummary
+): boolean => {
+  return (
+    s1.direction === s2.direction &&
+    s1.color === s2.color &&
+    s1.text === s2.text
+  );
+};
+
+const containsEquivalent = (
+  summaries: RTypeSummary[],
+  summary: RTypeSummary
+): boolean => {
+  for (let s of summaries) if (areSummariesEquivalent(s, summary)) return true;
+  return false;
+};
+
 /**
  * Should render something even if props.edges is empty.
  * To keep height consistent and show the loading state below.
  * @extends React
  */
 const BigLinksPreview: React.FunctionComponent<Props> = props => {
-  const { edges, sourceKey, targetKey } = props;
-  const { t } = useTranslation();
+  const { edges, sourceKey } = props;
 
-  // Find unique types
-  // const types = [];
-  // for (let i = 0; i < edges.length; i++) {
-  //   const link = edges[i];
-  //   if (types.indexOf(link.type) < 0) types.push(link.type);
-  // }
-  // types.sort((a, b) => a - b);
-
-  // TODO : de-dup. Better presentation. Responsive
+  const summaries: RTypeSummary[] = [];
+  for (let edgeKey in edges) {
+    const summary = getSummary(edges[edgeKey], sourceKey);
+    if (!containsEquivalent(summaries, summary)) summaries.push(summary);
+  }
+  summaries.sort((a, b) => a.type - b.type);
 
   return (
     <ExplainersWrapper className={props.className}>
-      {Object.keys(edges).map(key => {
-        const edge = edges[key];
-        const directed = isDirectedType(edge.type, edge.fType);
-        return (
-          <EdgeTypeExplainer key={key} color={edgeColor(edge.type, edge.fType)}>
-            {directed && edge._from === sourceKey && "⬅ "}
-            {t(edgeTextResKey(edge.type, edge.fType))}
-            {directed && edge._to === sourceKey && " ➡"}
-          </EdgeTypeExplainer>
-        );
-      })}
+      {summaries.map(s => (
+        <EdgeTypeExplainer key={s.key} color={s.color}>
+          {s.direction === LinkDir.Invert && <span className="left">⬅ </span>}
+          {s.text}
+          {s.direction === LinkDir.Normal && <span className="right"> ➡</span>}
+        </EdgeTypeExplainer>
+      ))}
     </ExplainersWrapper>
   );
 };
